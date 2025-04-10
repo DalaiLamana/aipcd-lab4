f06f7a1a0958ed4fe60145fc900bc5ea88a3c9a6
Lamana Garcia Mochales <daniel.lamana@c5c7.upc.es>
2025-04-10 14:50:45 +0200

    14.2 hecho

--- top_dir/uvm_tb/top_env_config.svh
+++ top_dir/uvm_tb/top_env_config.svh
@@ -29,7 +29,7 @@ function void top_env_config::initialize;
         addr_map_entry_s addr_map_entries[] = new [4];
         addr_map_entries = '{'{MAP_NORMAL,"CTRL",0,MAP_NS,4'h0,64'h0,64'h0004,MEM_NORMAL,MAP_NORM_SEC_DATA},'{MAP_NORMAL,"STATUS",0,MAP_NS,4'h0,64'h4,64'h0004,MEM_NORMAL,MAP_NORM_SEC_DATA},'{MAP_NORMAL,"IN_BLOCK",0,MAP_NS,4'h0,64'h8,64'h0200,MEM_NORMAL,MAP_NORM_SEC_DATA},'{MAP_NORMAL,"OUT_BLOCK",0,MAP_NS,4'h0,64'h208,64'h0200,MEM_NORMAL,MAP_NORM_SEC_DATA}};
         DCT = address_map::type_id::create("DCT_addr_map");
-        DCT.addr_mask = 64'hFFF;
+        DCT.addr_mask = 64'h3;
         DCT.set( addr_map_entries );
     end
 endfunction: initialize
